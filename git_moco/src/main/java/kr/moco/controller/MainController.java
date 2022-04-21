d62d3397e64d345f7c9da92c13d8652a51337ac7
smhrd <smhrd@DESKTOP-1JU6375>
2022-04-21 17:19:29 +0900

    MainController.java에 @RequestMapping으로 경로 설정

--- git_moco/src/main/java/kr/moco/controller/MainController.java
+++ git_moco/src/main/java/kr/moco/controller/MainController.java
@@ -2,6 +2,7 @@ package kr.moco.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // -> 이걸 붙여줘야 이 클래스가 POJO가 된다.(전처리 기호)
 // jsp나 redirect로 응답할 때 사용하는 Controller
@@ -12,4 +13,23 @@ public class MainController {
 		return "main"; // jsp로 가는 경로
 	}
 	
+	@RequestMapping("/product.do")
+	public String productPage() {
+		return "moco_cam";
+	}
+
+	@RequestMapping("/complain.do")
+	public String complainPage() {
+		return "complain";
+	}
+
+	@RequestMapping("/faq.do")
+	public String faq() {
+		return "faq";
+	}
+
+	@RequestMapping("/guide.do")
+	public String guidePage() {
+		return "mocoGuide";
+	}
 }
